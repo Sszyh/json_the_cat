615069788d1fd7121ffa7f0ff48179d022ddc310
Sszyh <zhuyer@outlook.com>
2022-07-29 13:56:16 -0700

    add comments

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const { fetchBreedDescription } = require('./breedFetcher');
 const args = process.argv;
 const breedName = args[2];
 fetchBreedDescription(breedName, (error, desc) => {
+  //if statement handle error like wrong breed name
   if (error) {
     console.log('Error fetch details:', error);
   } else {
