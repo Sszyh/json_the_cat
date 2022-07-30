0e69aae07e3b9b1165487554b3cfaccbb755222b
Sszyh <zhuyer@outlook.com>
2022-07-29 17:12:39 -0700

    eslint file

--- breedFetcher.js
+++ breedFetcher.js
@@ -3,11 +3,11 @@ const request = require('request');
 const fetchBreedDescription = function(breedName, callback) {
   let url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
   request(url, function(error, response, body) {
-    //if statement handle request error 
+    //if statement handle request error
     if (error) {
       callback(error,null);
     } else {
-      //parse "body" by JSON 
+      //parse "body" by JSON
       const data = JSON.parse(body);
       if (data.length === 0) {
         callback('do not have this breed',null);
