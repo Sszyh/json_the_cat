deb1fc6011a7c4242d7b5baeef88691c50d3ca7a
Sszyh <zhuyer@outlook.com>
2022-07-29 17:44:53 -0700

    add comments

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const { fetchBreedDescription } = require('./breedFetcher');
 const args = process.argv;
 const breedName = args[2];
+
+//call the function and set a anonymous callback
 fetchBreedDescription(breedName, (error, desc) => {
   //if statement handle error like wrong breed name
   if (error) {
